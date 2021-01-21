f4e186b59d805945ca918a09fd89d40d0f8b51aa
zoeengel <zengel1706@gmail.com>
2021-01-21 16:05:43 +0200

    thursday

--- login.py
+++ login.py
@@ -1,31 +1,46 @@
+# ZOE ANNASTASIA ENGEL
+# CLASS 1
+
 from tkinter import *
 import mysql.connector
 import tkinter as tk
 from tkinter import messagebox
 from datetime import *
 
+# IMPORTING THE MYSQL DATABASE
 mydb = mysql.connector.connect(user='lifechoices', passwd='@Lifechoices1234', db='lifechoicesonline', host='127.0.0.1', auth_plugin='mysql_native_password')
 mycursor = mydb.cursor()
 
+# CREATING MAIN LOGIN WINDOW
 window = tk.Tk()
 window.title('Life Choices online')
-window.geometry('450x220')
-lblusername = Label(window,text = 'Username:')
+window.geometry('460x230')
+window.resizable(False, False)
+lblusername = Label(window, text='Username:')
 lblusername.place(x=30, y=30)
 userent = Entry(window)
-userent.place(x=150,y=30)
+userent.place(x=150, y=30)
 
-lblpsswrd = Label(text = 'Password:')
+lblpsswrd = Label(text='Password:')
 lblpsswrd.place(x=30, y=80)
-userpsswrd = Entry(window, show= '*')
-userpsswrd.place(x=150,y=78)
+userpsswrd = Entry(window, show='*')
+userpsswrd.place(x=150, y=78)
 
 
+# QUIT WINDOW FUNCTION
+def exit():
+    ex = messagebox.askquestion('Exit Application', 'Are you sure you want to exit the application?', icon='warning')
+    if ex == 'yes':
+        window.destroy()
+    else:
+        pass
+
+# NEW TKINTER PAGE TO REGISTER A NEW USER/VISITOR
 def registerUser():
-    register= Tk()
+    register = Tk()
     register.title('Register User')
     register.geometry('450x230')
-    register.resizable(False,False)
+    register.resizable(False, False)
 
     fllname_lbl = Label(register, text="Full name:")
     fllname_ent = Entry(register)
@@ -42,6 +57,7 @@ def registerUser():
     psswrd_lbl.place(x=30, y=110)
     pssrwd_ent.place(x=150, y=110)
 
+    # FUNCTION TO CREATE THE NEW USER/VISITOR
     def createUser():
         try:
             credentials = fllname_ent.get(), usrname_ent.get(), pssrwd_ent.get()
@@ -57,43 +73,50 @@ def registerUser():
     createusrbtn.place(x=120, y=170)
 
 
+# LOGIN FUNCTION WHICH TRACKS THE SIGN-IN/SIGN-OUT TIME
 def login():
     try:
         loginsql = "select * from Users where Username = %s and Password=%s"
-        mycursor.execute(loginsql,[(userent.get()), (userpsswrd.get())])
+        mycursor.execute(loginsql, [(userent.get()), (userpsswrd.get())])
 
         usersdb = mycursor.fetchall()
         if usersdb:
-            messagebox.showinfo('Message','Logged in successfully')
+            messagebox.showinfo('Message', 'Logged in successfully')
+        else:
+            messagebox.showinfo('Error','Incorrect Password/Username')
+            quit()
             master = Tk()
             master.title("Sign-in Sign-out")
             master.geometry("250x100")
             timeIn = datetime.now()
             x = timeIn.strftime("%H:%M:%S")
+
+            # POPS UP THE SIGN UP PAGE WITH THE SIGN OUT BUTTON
             def signout():
                 timeout = datetime.now()
                 y = timeout.strftime("%H:%M:%S")
                 z = userent.get()
-
                 infoTime = z, x, y
-
                 timeComm = "INSERT INTO Time_Register(Username, Sign_in, Sign_out) VALUES(%s, %s, %s)"
 
                 mycursor.execute(timeComm, infoTime)
 
                 mydb.commit()
-                messagebox.showinfo('Message','Signed out!')
+                messagebox.showinfo('Message', 'Signed out!')
                 master.destroy()
-            sign_outbtn= Button(master, text="Sign out", command=signout ,bg="green", fg="white")
+
+            sign_outbtn = Button(master, text="Sign out", command=signout, bg="green", fg="white")
             sign_outbtn.place(x=90, y=30)
             master.mainloop()
     except:
         pass
-def createAdmin():
-    root=Tk()
-    root.title("Create Admin/User")
-    root.geometry('450x230')
 
+
+def createAdmin():
+    root = Tk()
+    root.title("Admin login")
+    root.geometry('450x230')
+    root.resizable(False,False)
 
     fllname_lbl = Label(root, text="Full name:")
     fllname_ent = Entry(root)
@@ -121,13 +144,30 @@ def createAdmin():
         except:
             messagebox.showerror("ERROR", "User already exists")
 
+    def priv():
+        u = usrname_ent.get()
+        x = ("GRANT ALL PRIVILEGES ON *.* TO 'username = %s'@'localhost' VALUES(%s)") %(u)
+        mycursor.execute(x)
+        mycursor.fetchall()
+    pribtn = Button(root, text="Grant privaleges", command=priv, bg="blue", fg="white")
+    pribtn.place(x=240, y=170)
     createadminusrbtn = Button(root, text="Create user", command=createSuperUser, bg="green", fg="white")
-    createadminusrbtn.place(x=120, y=170)
+    createadminusrbtn.place(x=125, y=170)
+    admin_login= Button(root, text="Login")
+    admin_login.place(x=50, y=170)
 
-lgnbtn = Button(window, text="Login" ,command=login , bg="green", fg="white")
+# CREAING A HOT KEY (PRESS Cntrl+a)
+def key():
+    createAdmin()
+window.bind("<Control-a>", lambda x: key())
+
+lgnbtn = Button(window, text="Login", command=login, bg="green", fg="white")
 lgnbtn.place(x=160, y=140)
-rgstbtn = Button(window, text="Register", command=registerUser, bg="green", fg="white")
+rgstbtn = Button(window, text="Register New User", command=registerUser, bg="green", fg="white")
 rgstbtn.place(x=260, y=140)
-admin_btn = Button(window, text="Create admin/user", bg="grey", fg="white" , command=createAdmin)
-admin_btn.place(x=160, y=180)
+close_btn = Button(window, text='Quit', bg='red', command=exit)
+close_btn.place(x=396, y=190)
 window.mainloop()
+
+# GRANT PRIVILAGES TO ADMIN USER, MUST VIEW DATABASE TABLES, CREATE/DELETE USER FROM USERS TABLE
+# Login As admin and view tables
