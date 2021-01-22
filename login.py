d3fb8b5afee9cf494cc2a6255096695fe50467a8
Zoe Engel <71072069+zoeengel@users.noreply.github.com>
2021-01-22 16:21:25 +0200

    Add files via upload

--- login.py
+++ login.py
@@ -1,6 +1,7 @@
 # ZOE ANNASTASIA ENGEL
 # CLASS 1
 
+# TO ACCESS ADMIN PAGE PRESS CNTRL+a KEYS
 from tkinter import *
 import mysql.connector
 import tkinter as tk
@@ -8,7 +9,8 @@ from tkinter import messagebox
 from datetime import *
 
 # IMPORTING THE MYSQL DATABASE
-mydb = mysql.connector.connect(user='lifechoices', passwd='@Lifechoices1234', db='lifechoicesonline', host='127.0.0.1', auth_plugin='mysql_native_password')
+mydb = mysql.connector.connect(user='lifechoices', passwd='@Lifechoices1234', db='lifechoicesonline', host='127.0.0.1',
+                               auth_plugin='mysql_native_password')
 mycursor = mydb.cursor()
 
 # CREATING MAIN LOGIN WINDOW
@@ -34,6 +36,7 @@ def exit():
         window.destroy()
     else:
         pass
+
 
 # NEW TKINTER PAGE TO REGISTER A NEW USER/VISITOR
 def registerUser():
@@ -82,14 +85,12 @@ def login():
         usersdb = mycursor.fetchall()
         if usersdb:
             messagebox.showinfo('Message', 'Logged in successfully')
-        else:
-            messagebox.showinfo('Error','Incorrect Password/Username')
-            quit()
             master = Tk()
             master.title("Sign-in Sign-out")
             master.geometry("250x100")
             timeIn = datetime.now()
             x = timeIn.strftime("%H:%M:%S")
+
 
             # POPS UP THE SIGN UP PAGE WITH THE SIGN OUT BUTTON
             def signout():
@@ -100,7 +101,6 @@ def login():
                 timeComm = "INSERT INTO Time_Register(Username, Sign_in, Sign_out) VALUES(%s, %s, %s)"
 
                 mycursor.execute(timeComm, infoTime)
-
                 mydb.commit()
                 messagebox.showinfo('Message', 'Signed out!')
                 master.destroy()
@@ -112,11 +112,12 @@ def login():
         pass
 
 
+# ADMINS LOGIN PAGE
 def createAdmin():
     root = Tk()
     root.title("Admin login")
     root.geometry('450x230')
-    root.resizable(False,False)
+    root.resizable(False, False)
 
     fllname_lbl = Label(root, text="Full name:")
     fllname_ent = Entry(root)
@@ -144,17 +145,111 @@ def createAdmin():
         except:
             messagebox.showerror("ERROR", "User already exists")
 
-    def priv():
-        u = usrname_ent.get()
-        x = ("GRANT ALL PRIVILEGES ON *.* TO 'username = %s'@'localhost' VALUES(%s)") %(u)
-        mycursor.execute(x)
-        mycursor.fetchall()
-    pribtn = Button(root, text="Grant privaleges", command=priv, bg="blue", fg="white")
-    pribtn.place(x=240, y=170)
+    def loginAdmin():
+        try:
+            adminlgn= "select * from Admin where Username = %s and Password=%s"
+            mycursor.execute(adminlgn, [(usrname_ent.get()), (pssrwd_ent.get())])
+            usersdb = mycursor.fetchall()
+            if usersdb:
+                messagebox.showinfo('Message', 'Logged in successfully')
+                table = Tk()
+                table.title("Database tables")
+                table.geometry("880x500")
+                table.resizable(False,False)
+
+                # SHOWING USERS IN A LIST BOX
+                ID = Label(table, text="ID")
+                Fulln = Label(table, text="Full name")
+                Usernm = Label(table, text="Username")
+                Passwrd = Label(table, text="Password")
+
+                IDList = Listbox(table, width=25)
+                FullnmList = Listbox(table, width=25)
+                UsernmList = Listbox(table, width=25)
+                PasswrdList = Listbox(table, width=25)
+
+                # SHOWING SIGN IN/SIGN OUT TIMES IN A LIST BOX
+                Username = Label(table, text="Username")
+                Signin = Label(table, text="Sign in")
+                Signout = Label(table, text="Sign out")
+
+                Usernamelist = Listbox(table, width=25)
+                Signlist = Listbox(table, width=25)
+                Signoutlist = Listbox(table, width=25)
+
+                # FOR LOOPS (USERS LIST BOX)
+                mycursor.execute("SELECT ID from Users")
+                tab = mycursor.fetchall()
+
+                for i in tab:
+                    IDList.insert(END, i)
+
+                mycursor.execute("SELECT Full_name from Users")
+                tab = mycursor.fetchall()
+
+                for i in tab:
+                    FullnmList.insert(END, i)
+
+                mycursor.execute("SELECT Username from Users")
+                tab = mycursor.fetchall()
+
+                for i in tab:
+                    PasswrdList.insert(END, i)
+
+                mycursor.execute("SELECT Password from Users")
+                tab = mycursor.fetchall()
+
+                for i in tab:
+                    UsernmList.insert(END, i)
+
+                # FOR LOOPS FOR TIME REGISTER
+                mycursor.execute("SELECT Username from Time_Register")
+                tab = mycursor.fetchall()
+
+                for i in tab:
+                    Usernamelist.insert(END, i)
+
+                mycursor.execute("SELECT Sign_in from Time_Register")
+                tab = mycursor.fetchall()
+
+                for i in tab:
+                    Signlist.insert(END, i)
+
+                mycursor.execute("SELECT Sign_out from Time_Register")
+                tab = mycursor.fetchall()
+
+                for i in tab:
+                    Signoutlist.insert(END, i)
+
+                ID.place(x=5, y=4)
+                IDList.place(x=5,y=20)
+                Fulln.place(x=225, y=4)
+                FullnmList.place(x=225, y=20)
+                Usernm.place(x=445, y=4)
+                UsernmList.place(x=445, y=20)
+                Passwrd.place(x=670, y=4)
+                PasswrdList.place(x=670, y=20)
+
+                Username.place(x=5, y=210)
+                Usernamelist.place(x=5, y=230)
+                Signin.place(x=225, y=210)
+                Signlist.place(x=225, y=230)
+                Signout.place(x=445, y=210)
+                Signoutlist.place(x=445, y=230)
+
+                table.mainloop()
+            else:
+                messagebox.showinfo('Error', 'Incorrect Password/Username')
+                quit()
+        except:
+            pass
+
     createadminusrbtn = Button(root, text="Create user", command=createSuperUser, bg="green", fg="white")
     createadminusrbtn.place(x=125, y=170)
-    admin_login= Button(root, text="Login")
+    # LOGIN BUTTON
+    admin_login = Button(root, text="Login", command=loginAdmin)
     admin_login.place(x=50, y=170)
+
 
 # CREAING A HOT KEY (PRESS Cntrl+a)
 def key():
